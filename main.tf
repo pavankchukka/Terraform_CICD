78956614161ed20d05dd2699da36d2d65d7b8bd2
pavankchukka <162408999+pavankchukka@users.noreply.github.com>
2024-03-23 12:35:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "dev" {
     ami = "ami-026255a2746f88074"
-    instance_type = "t2.medium"
+    instance_type = "t2.micro"
     tags = {
       Name = "dev-ec2"
     }
